3ed5e04f5aa08f208e42ae24d9f005e10c9e1c51
juliantejera <juliantejera@outlook.com>
2018-06-06 17:29:20 -0700

    Initial Commit

---
+++ JTDataStructures/JTDataStructures.h
@@ -0,0 +1,19 @@
+//
+//  JTDataStructures.h
+//  JTDataStructures
+//
+//  Created by Julian Tejera-Frias on 6/6/18.
+//  Copyright © 2018 Julian Tejera-Frias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JTDataStructures.
+FOUNDATION_EXPORT double JTDataStructuresVersionNumber;
+
+//! Project version string for JTDataStructures.
+FOUNDATION_EXPORT const unsigned char JTDataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTDataStructures/PublicHeader.h>
+
+
