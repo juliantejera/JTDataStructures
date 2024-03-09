bd6bef453179a72e331d59b76c3a6b27446dc4b9
Julian Tejera-Frias <jtejera27@gmail.com>
2024-03-09 10:50:23 -0500

    Updated Trie method signature and added BST

---
+++ JTDataStructures/Trees/BInarySearchTree.swift
@@ -0,0 +1,9 @@
+//
+//  BInarySearchTree.swift
+//  JTDataStructures
+//
+//  Created by Julian Tejera-Frias on 2/2/23.
+//  Copyright © 2023 Julian Tejera-Frias. All rights reserved.
+//
+
+import Foundation
---
+++ JTDataStructuresTests/Helpers/CustomAssertions.swift
@@ -0,0 +1,9 @@
+//
+//  CustomAssertions.swift
+//  JTDataStructuresTests
+//
+//  Created by Julian Tejera-Frias on 2/2/23.
+//  Copyright © 2023 Julian Tejera-Frias. All rights reserved.
+//
+
+import Foundation
